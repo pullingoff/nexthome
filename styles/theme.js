efbdbac346f4f35e3dac5f638dca1feca37e6084
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-03-24 17:59:50 +0900

    👗 styles

---
+++ styles/theme.js
@@ -0,0 +1,10 @@
+const styledTheme = {
+    device: {
+      xs: '335px',  //mobile
+      sm: '758px',  //tablet
+      md: '980px',  //desktop
+      lg: '1441px',
+    },
+  };
+  
+  export default styledTheme;
