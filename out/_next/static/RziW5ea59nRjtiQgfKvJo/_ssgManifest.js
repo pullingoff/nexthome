80b4b9e104f9783fb3bf8a50fd24cebb53ebbb80
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-04-10 22:20:48 +0900

    deploy to gh-pages

---
+++ out/_next/static/RziW5ea59nRjtiQgfKvJo/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002F","\u002Fblog\u002Fpage\u002F[page_index]","\u002Ftags\u002F[tag]\u002Fpages\u002F[keyword]","\u002Fblog\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
