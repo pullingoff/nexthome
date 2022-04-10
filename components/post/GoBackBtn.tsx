3f7f46d5360c3d2c98d39af45b4f9d4d18bf0d2e
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-04-10 22:11:33 +0900

    Added

---
+++ components/post/GoBackBtn.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+
+type BtnProps = {
+    onClick: () => void;
+}
+
+const GoBackBtn = ({ ...props }: BtnProps) =>{
+    return(
+        <StyledGoBackBtn>
+            &larr; 이전
+        </StyledGoBackBtn>    
+        
+    )
+}
+
+export default GoBackBtn
+
+
+const StyledGoBackBtn = styled.button`
+display: block;
+margin: 10px 0;
+color: var(--color-point-blue);
+font-weight: 700;
+&:hover {
+  color: var(--hover-color);
+}
+`
