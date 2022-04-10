ef75fc86fb4a996f9648b4a4d7b2973416149fdd
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-04-10 22:11:08 +0900

    add loading
    1. add loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,30 @@ import '../styles/globals.scss';
 import Layout from '../components/Layout';
 import MetaContainer from '../components/MetaContainer';
 import { AppProps } from 'next/app';
-
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Loading from 'components/Loading';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      url !== router.pathname ? setLoading(true) : setLoading(false);
+    };
+    const handleComplete = (url: string) => setLoading(false);
+  
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+  }, [router]);
+
   return (
     <Layout>
       <MetaContainer/>
+      <Loading loading={loading}/>
       <Component {...pageProps} />
     </Layout>
   )
