81a6bb74993277169e93a77dbba55d1c892de9dc
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-04-11 22:37:56 +0900

    Create ProjectList.ts

---
+++ utils/ProjectList.ts
@@ -0,0 +1,18 @@
+import { IProject } from "type";
+
+export const ProjectList : Array<IProject> = [
+    {
+        title : 'Next.js Blog',
+        slug: 'pjnexthome',
+        type : 'Webapp',
+        thumbnail : 'nexthome.png',
+        shortDescription: 'Next.js로 만들어본 블로그'
+    },  
+    {
+        title : 'JS Quiz',
+        slug: 'jsquiz',
+        type : 'Webapp',
+        thumbnail: 'nexthome.png',
+        shortDescription : '자바스크립트 관련 문제를 풀어볼 수 있어요.'
+    },  
+]
