9bb6d2cba15875bc0150037c112da1dc1218b39f
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-04-15 19:19:43 +0900

    Create ImgGallery.tsx

---
+++ components/ImgGallery.tsx
@@ -0,0 +1,57 @@
+import styled from "styled-components"
+import Image from "next/image"
+import path from 'path'
+
+const ImgGallery = ({imgList} : {
+    imgList : string[]
+}) => {
+    const imgCnt = imgList.length;
+    const imgDir = path.join('images', 'pjDetail')
+    return (
+        <BlockContainer>
+            {imgList && imgList.map((img, idx) => (
+                <ImgItem key={idx}>
+                    <StyledThumb priority
+                            layout="fill"
+                            objectFit="contain"
+                            src={`/${path.join(imgDir, img)}`}
+                            alt={`${idx} 사진`}/>
+                </ImgItem>
+            ))}
+        </BlockContainer>
+    )
+}
+
+const FlexContainer = styled.div`
+display: flex;
+width: 140%;
+overflow-x: scroll;
+flex-direction: row;
+justify-content: space-around;
+position: relative;
+left: -30px;
+margin-bottom: 20px;
+`
+
+const BlockContainer = styled.div`
+display: block;
+overflow: visible;
+margin-bottom: 20px; 
+@media (min-width: ${({theme}) => theme.device.sm}) {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    overflow: initial;
+    margin: 0 10vw 40px;
+}
+`
+const StyledThumb = styled(Image)`
+    position: relative;
+`
+
+const ImgItem = styled.section`
+position: relative;
+width: 100%;
+min-height: 200px;
+`
+export default ImgGallery
