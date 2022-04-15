f26d6a1dfa4984b786dbf418cae2df65d720b788
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-04-15 14:25:21 +0900

    add devicesizes for img

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  images: { 
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840], 
+  },
+
 }
 
 module.exports = nextConfig
