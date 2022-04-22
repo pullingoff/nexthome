c6d45104afae580366d003e5d3fa1d595da182c1
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-04-22 16:59:32 +0900

    changed var name and added underline animation

--- components/nav/NavMenu.tsx
+++ components/nav/NavMenu.tsx
@@ -26,14 +26,33 @@ export default NavMenu
 const StyledMenu = styled.li`
 display: inline-block;
 margin-left: 1.5rem; 
-color: var(--theme2-color);
+color: var(--theme1-color);
 font-size:25px;
 font-weight: 600;
+
 & > a{
+    display: block;
+    position: relative;
+    &:after {
+        content: '';
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 0.1rem;
+        background-color: salmon;
+        opacity: 0;
+        transition: opacity 300ms, transform 300ms;
+        transform: scale(0);
+        transform-origin: center;
+    }
     &:hover {
-        color: var(--color-point-pink);
-        transition: all .4s ease;
-        -webkit-transition: all .4s ease;
+        color: salmon;
+        &:after {
+            opacity: 1;
+            transform: translate3d(0, 0.2em, 0);
+            transform: scale(1);   
+        }
     }
 }
 `
