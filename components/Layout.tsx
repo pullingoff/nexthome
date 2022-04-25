32c7183d402ce39c4450723a3d1817d07cc1afc0
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-04-25 18:47:22 +0900

    css change

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -22,7 +22,7 @@ export default Layout
 const Content = styled.main`
 max-width: var(--breakpoint-desktop);
 margin: auto;
-padding: 10px 20px;
+padding: 10px 30px;
 background: var(--bg-yellow);
 border-bottom: 1px solid lightgrey;
 min-height: 80vh;
