102485f4f37ee1ad57bfea93dc2978757d1b2b8b
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-04-28 16:08:18 +0900

    type changed

--- components/project/ArticleContainer.tsx
+++ components/project/ArticleContainer.tsx
@@ -5,21 +5,29 @@ import ImgGallery from "components/ImgGallery";
 const ArticleContainer = ({pj} :{
     pj: IProject
 }) => {
+    
     return (
         <Container>
-            <LongDes>{pj.longDescription}</LongDes>
+            {pj.longDescription &&
+            pj.longDescription.map((p, idx)=> (
+                <LongDes key={idx}>
+                    {p}
+                </LongDes>
+            )) }
         </Container>
     )
 }
 
 
-const LongDes = styled.pre`
-font-size: 1.2rem;
+const LongDes = styled.p`
+
 line-height: 170%;
 white-space: normal;
+margin: 0.5rem 0;
 
 @media (min-width: ${({theme}) => theme.device.md}) {
-    max-width: 50%;
+    // max-width: 50%;
+    font-size: 1.2rem;
 }
 `
 
@@ -29,7 +37,7 @@ padding: 10px;
 
 @media (min-width: ${({theme}) => theme.device.sm}) {
     margin:  0 5vw 5vw 10vw;
-    min-height: 300px;
+    // min-height: 300px;
     padding: 30px 50px 0;
 }
 
