37d8e2034b6ef9b3657eb79d4244c15431d1ec24
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-05-10 22:39:36 +0900

    동작 확인 후 주석처리
    개발 중

--- components/about/AboutContainer.tsx
+++ components/about/AboutContainer.tsx
@@ -11,7 +11,7 @@ export default function AboutContainer() {
     return (
         <StyledAboutContainer>
             <GridHeader/>
-            <ProjectContainer/>
+            {/* <ProjectContainer/> */}
             <WorkExpContainer/>
             <OtherExperiences/>
             <TechStack/>
