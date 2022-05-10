7a56d4652a7dae6654f1ce95b651654aeb33ab03
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-05-10 22:34:30 +0900

    add logocontainer

--- components/SocialMediaLogos.tsx
+++ components/SocialMediaLogos.tsx
@@ -2,7 +2,32 @@ import styled from "styled-components"
 import CustomLink from "./CustomLink"
 import { METADATA } from "@config/index"
 import { GithubLogo, LinkedInLogo, TwitterLogo } from "public/logos"
+import { ReactNode } from "react"
 
+const LogoContainer = ({href, children}: {
+   href: string
+   children: ReactNode
+}) => {
+   return (
+      <LogoCircle>
+         <CustomLink href={href}>
+            {children}
+         </CustomLink>
+      </LogoCircle>
+   )
+}
+
+const LogoCircle = styled.div`
+content: '';
+       display: inline-block;
+       width: 30px;
+       height: 30px;
+       -moz-border-radius: 15px;
+       -webkit-border-radius: 15px;
+       border: 1px solid var(--theme1-color);
+       border-radius: 15px;
+    }
+`
 const SocialMediaLogos = () => {
     const {github, twitter, email, linkedinLink} = METADATA
 
