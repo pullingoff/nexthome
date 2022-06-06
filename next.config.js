de58e67658d4d293234165c9bd09e3480230ce3f
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-06-06 15:09:02 +0900

    디폴트 로케일 설정

--- next.config.js
+++ next.config.js
@@ -11,7 +11,10 @@ const nextConfig = {
   images: { 
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840], 
   },
-
+  i18n: {
+    locales: ["kr"],
+    defaultLocale: "kr",
+  },
 }
 
 module.exports = nextConfig
