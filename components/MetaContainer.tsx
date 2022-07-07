f2f4d9688f2675055922f05760f0cab9b72a4d14
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-07-07 21:56:09 +0900

    fix : meta tag 중복 안 나도록 key를 둠

--- components/MetaContainer.tsx
+++ components/MetaContainer.tsx
@@ -7,22 +7,18 @@ const MetaContainer = ({ customMeta }: { customMeta?: ICustomMeta }) => {
     ...METADATA, // 기본값
     ...customMeta, // 커스텀 메타데이터
   };
-
+  // console.log(customMeta);
   return (
     <Head>
-      <title>{config.title}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="robots" content="follow, index" />
-      <meta content={config.description} name="description" />
-      <meta property="og:type" content="website" />
-      <meta property="og:site_name" content={config.title} />
-      <meta property="og:description" content={config.description} />
-      <meta property="og:title" content={config.title} />
-      {/* <meta property="og:image" content={config.image} /> */}
+      <meta property="og:site_name" content={config.title} key="" />
+      <meta property="og:title" content={config.title} key="" />
+      <title>{config.title}</title>
+      <meta content={config.description} name="description" key="" />
+      <meta property="og:description" content={config.description} key="" />
       {config.date && (
         <meta property="article:published_time" content={config.date} />
       )}
-      <link rel="icon" href="/favicon.ico" />
     </Head>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,41 +3,21 @@ import Layout from '@components/layout/Layout';
 import MetaContainer from '@components/MetaContainer';
 import { AppProps } from 'next/app';
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
 import Loading from 'components/Loading';
 import styled from 'styled-components';
-import * as gtag from '../lib/gtag';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const handleStart = (url: string) => {
-      url !== router.pathname ? setLoading(true) : setLoading(false);
-    };
-    const handleComplete = () => setLoading(false);
-
-    router.events.on('routeChangeStart', handleStart);
-    router.events.on('routeChangeComplete', handleComplete);
-    router.events.on('routeChangeError', handleComplete);
-  }, [router]);
-
+    setLoading(false);
+  }, []);
+  console.log(pageProps);
   // next/router를 이용해서 넥스트앱에서 라우팅이 발생됐을 때
   // 강제적으로 gtag의 pageview 함수로 구글 애널리틱스에게 다른 페이지도 봤다고 알리는 겁니다.
-  useEffect(() => {
-    const handleRouteChange = (url: any) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
 
   return (
     <StyledLayout>
-      {/* <DefaultSeo {...SEO} /> */}
       <MetaContainer />
       <Loading loading={loading} />
       <Component {...pageProps} />
