c9c2dbfbb9e156b6af304c48b4b358d407783975
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-07-10 22:18:39 +0900

    next.js fs 웹팩 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,20 +1,26 @@
 /** @type {import('next').NextConfig} */
 
-const path = require('path')
+const path = require('path');
 
 const nextConfig = {
   reactStrictMode: true,
-  poweredByHeader: process.env.NODE_ENV === 'development',  
+  poweredByHeader: process.env.NODE_ENV === 'development',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  images: { 
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840], 
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   i18n: {
-    locales: ["kr"],
-    defaultLocale: "kr",
+    locales: ['kr'],
+    defaultLocale: 'kr',
   },
-}
+  // webpack5: true,
+  webpack: config => {
+    config.resolve.fallback = { fs: false };
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
