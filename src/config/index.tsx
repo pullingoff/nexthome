812834e6299f0b5e82c0f09785ac973257bbe713
haeunFassto <haeun.park@fssuniverse.com>
2022-08-21 16:15:42 +0900

    프로젝트 숨김, 간단한 텍스트 수정

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -15,7 +15,7 @@ export const POSTS_PER_PAGE = 8;
 // nav bar에 나타날 메뉴
 export const MENUS = [
   { title: '기록', path: '/blog/page/1', emoji: '📝' },
-  { title: '프로젝트', path: '/project', emoji: '🎞' },
+  // { title: '프로젝트', path: '/project', emoji: '🎞' },
   { title: '이력서', path: '/about', emoji: '🕷' },
   { title: 'TIL', path: 'https://pullingoff.github.io', emoji: '📓' },
 ];
