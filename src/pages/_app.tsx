058541f4239666dde73475e997c49182a4b7dc57
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-09-03 22:11:08 +0900

    되는시간 연결

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,8 @@ import { AppProps } from 'next/app';
 import { useState, useEffect } from 'react';
 import Loading from '#components/Loading';
 import styled from 'styled-components';
+import hifiImg from '#public/hifi.png';
+import Image from 'next/image';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState(false);
@@ -18,9 +20,40 @@ function MyApp({ Component, pageProps }: AppProps) {
       <MetaContainer />
       <Loading loading={loading} />
       <Component {...pageProps} />
+      <CoffeeChat />
     </StyledLayout>
   );
 }
+
+const CoffeeChat = () => {
+  return (
+    <StyledCoffeeChatContainer>
+      <a href="https://whattime.co.kr/haeun" target="_blank">
+        <Image
+          src={hifiImg}
+          alt="Hifi 3D Illustration by Welson Hendra"
+          width={'50px'}
+          height={'50px'}
+        />
+      </a>
+    </StyledCoffeeChatContainer>
+  );
+};
+
+const StyledCoffeeChatContainer = styled.div`
+  position: fixed;
+  right: 3%;
+  bottom: 4%;
+  cursor: pointer;
+  border: 1px solid var(--theme1-color);
+  background: white;
+  border-radius: 50%;
+  padding: 0.3rem;
+  &:hover {
+    transform: scale(1.2, 1.1);
+    box-shadow: 0 30px 45px -15px var(--theme1-color);
+  }
+`;
 
 const StyledLayout = styled(Layout)`
   background: var(--bg-yellow);
