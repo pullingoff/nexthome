d142df1d1b86df2bfa2def43059cbd4832a9b26e
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2023-01-08 12:37:28 +0900

    MailLink 컴포넌트명 수정 잘못 되어있었음 수정

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,7 +2,11 @@ import Navbar from '../nav/Navbar';
 import styled, { ThemeProvider } from 'styled-components';
 import styledTheme from '#src/styles/theme';
 import { ReactNode } from 'react';
-import Footer from './Footer';
+import { METADATA } from '#src/config';
+import CustomLink from '#components/common/CustomLink';
+import SnsLogoBox from '#components/layout/SnsLogoBox';
+
+const S: any = {};
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
@@ -22,4 +26,36 @@ const Content = styled.main`
   padding: 25px;
   background: var(--bg-yellow);
   min-height: 70vh;
+`;
+
+const Footer = () => {
+  return (
+    <S.Footer>
+      <SnsLogoBox />
+      <S.MailLink href={`mailto:${METADATA.email}`}>
+        Email: {METADATA.email}
+      </S.MailLink>
+    </S.Footer>
+  );
+};
+
+S.Footer = styled.footer`
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  padding: var(--10xl) 0 var(--xl);
+  text-align: center;
+  margin: var(--xl) auto;
+  border-top: 1px solid var(--theme1-color);
+
+  @media (max-width: ${({ theme }) => theme.device.sm}) {
+    margin: var(--md) auto;
+  }
+`;
+
+S.MailLink = styled(CustomLink)`
+  &:hover {
+    font-weight: bold;
+  }
 `;
