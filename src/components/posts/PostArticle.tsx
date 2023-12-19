0fe61e6db99a3774097980d67c25d8c5ffdd44e0
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2023-12-19 15:34:26 +0900

    chore[issue-38]: postContent (글 내용) styled -> 모듈scss로 분리

--- src/components/posts/PostArticle.tsx
+++ src/components/posts/PostArticle.tsx
@@ -1,12 +1,11 @@
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
 import MetadataBox from '../MetadataBox';
-import styled from 'styled-components';
 import { Heading, Post } from '#src/type';
 import { useRouter } from 'next/router';
 import Comments from '#components/posts/Comments';
 import Link from 'next/link';
+import styles from './post.module.scss'
 
-const S: any = {};
 const PostArticle = ({
   post,
   mdxSource,
@@ -30,9 +29,9 @@ const PostArticle = ({
       <div className='flex items-start justify-center'>
         <article className='max-w-[90vw]'>
           <PostHeader date={date} title={title} />
-          <S.ContentBox>
+          <article className={styles.postContent}>
             <MDXRemote {...mdxSource} />
-          </S.ContentBox>
+          </article>
           <span className='block mr-[-] ml-[-] text-[color:var(--color-point-blue)] font-bold cursor-pointer mt-[calc(2%] mb-[1px)] hover:text-[salmon]' onClick={() => router.back()}>&larr; 이전</span>
         </article>
         {headings && <PostHeadings headings={headings} />}
@@ -42,101 +41,6 @@ const PostArticle = ({
   );
 };
 export default PostArticle;
-
-S.ContentBox = styled.div`
-  &:before {
-    content: ' ';
-    display: block;
-    margin: 30px 2%;
-  }
-
-  margin: 20px 2vw 10px;
-  font-size: 18px;
-  word-break: break-word;
-
-  p strong,
-  li strong {
-    font-weight: bold;
-  }
-
-  a {
-    font-weight: 500;
-    text-decoration: underline;
-  }
-
-  a:hover {
-    color: salmon;
-  }
-
-  ul {
-    margin-bottom: 1rem;
-    line-height: 180%;
-
-    li {
-      list-style: none;
-      padding-left: 1rem;
-
-      &:before {
-        content: '- ';
-      }
-    }
-  }
-
-  ol {
-    margin-bottom: 1rem;
-    line-height: 180%;
-  }
-
-  p img {
-    margin: 15px 0;
-    max-width: 700px;
-    width: -webkit-fill-available;
-  }
-
-  h1 {
-    font-size: var(--8xl);
-  }
-
-  h2 {
-    font-weight: 800;
-    font-size: var(--6xl);
-  }
-
-  h3 {
-    font-size: var(--4xl);
-    font-weight: 700;
-  }
-
-  h4 {
-    font-size: var(--3xl);
-  }
-
-  h1,
-  h2,
-  h3 {
-    margin-top: 2rem;
-    margin-bottom: 1rem;
-    line-height: 150%;
-  }
-
-  h1,
-  h2 {
-    border-bottom: 1px solid rgb(211, 211, 211, 0.5);
-    padding-bottom: 1rem;
-  }
-
-  h2,
-  h3 {
-    &:before {
-      content: '# ';
-    }
-  }
-
-  // h2 바로 아래 p는 margin-top 제거
-  p:where(h2 + *):not(:where([class~='not-p'] *)) {
-    margin-top: 0;
-  }
-`;
 
 const PostHeader = ({ date, title }: { date: string; title: string }) => {
   return (
