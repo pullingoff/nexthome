e2db3c54186cf7b3df45e00db10d838d0fb94109
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2024-01-19 12:59:38 +0900

    fix[issue-72]: sitemap 생성 함수 수정

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,47 +1,19 @@
+// app/sitemap.js
+
 import { getAllPosts } from '../src/lib/posts';
 
-const URL = 'https://haeun.dev';
+const URL = 'https://claritydev.net';
 
-function generateSiteMap(posts) {
-  return `<?xml version="1.0" encoding="UTF-8"?>
-   <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
-     <!-- Add the static URLs manually -->
-     <url>
-       <loc>${URL}</loc>
-     </url>
-     <url>
-       <loc>${URL}/portfolio</loc>
-     </url>
-      <url>
-       <loc>${URL}/blog</loc>
-     </url>
-     ${posts
-       .map(({ id }) => {
-         return `
-           <url>
-               <loc>${`${URL}/blog/${id}`}</loc>
-           </url>
-         `;
-       })
-       .join('')}
-   </urlset>
- `;
+export default async function sitemap() {
+  const posts = (await getAllPosts()).map(({ slug, frontMatter }) => ({
+    url: `${URL}/blog/${slug}`,
+    lastModified: frontMatter.date,
+  }));
+
+  const routes = ['', '/about', '/blog'].map(route => ({
+    url: `${URL}${route}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  return [...routes, ...posts];
 }
-
-export async function getServerSideProps({ res }) {
-  const posts = getAllPosts();
-
-  // Generate the XML sitemap with the blog data
-  const sitemap = generateSiteMap(posts);
-
-  res.setHeader('Content-Type', 'text/xml');
-  // Send the XML to the browser
-  res.write(sitemap);
-  res.end();
-
-  return {
-    props: {},
-  };
-}
-
-export default function SiteMap() {}
